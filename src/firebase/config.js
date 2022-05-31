740b0c8c2ad33bacc2028e513acf3ce656b14ede
Trung Cao <caot1@nku.edu>
2022-05-30 22:59:30 -0400

    Add Firestore & Initialize Firebase rules

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC4U1qpjIvhvCTXIGebNR3OBt9p3Q4QALY",
+    authDomain: "work-hub-proj.firebaseapp.com",
+    projectId: "work-hub-proj",
+    storageBucket: "work-hub-proj.appspot.com",
+    messagingSenderId: "1047397205106",
+    appId: "1:1047397205106:web:2d5c87360f797ae7f12eeb"
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Initialize Services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// Set up timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export default { projectFirestore, projectAuth, timestamp }
